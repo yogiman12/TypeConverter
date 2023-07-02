1da92cb3e37f00916bec62ea84a5f02ffd9c2ec5
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-07-02 16:04:26 +0300

    make the code more light and add numbers

--- clip.py
+++ clip.py
@@ -1,20 +1,21 @@
-import pyperclip
-import pyautogui
-import keyboard
-import threading
+from pyperclip import copy , paste 
+from pyautogui import hotkey 
+from keyboard import add_hotkey , wait
+from threading import Thread
 
-EngToArKeyboard = {'`': 'ذ','q': 'ض','w': 'ص','e': 'ث','r': 'ق','t': 'ف','y': 'غ','u': 'ع','i': 'ه','o': 'خ','p': 'ح','[': 'ج',']': 'د',
+EngToArKeyboard = {'`': 'ذ','1': '1','2': '2','3': '3','4': '4','5': '5','6': '6','7': '7','8': '8','9': '9','0': '0','-': '-','=': '=',
+                        'q': 'ض','w': 'ص','e': 'ث','r': 'ق','t': 'ف','y': 'غ','u': 'ع','i': 'ه','o': 'خ','p': 'ح','[': 'ج',']': 'د',
                        'a': 'ش','s': 'س','d': 'ي','f': 'ب','g': 'ل','h': 'ا','j': 'ت','k': 'ن','l': 'م',';': 'ك',
-                       '\'': 'ئ','z': 'ئ','x': 'ء','c': 'ؤ','v': 'ر','b': 'ﻻ','n': 'ى','m': 'ة',',': 'و','.': 'ز','/': 'ظ',
+                       '\'': 'ط','z': 'ئ','x': 'ء','c': 'ؤ','v': 'ر','b': 'ﻻ','n': 'ى','m': 'ة',',': 'و','.': 'ز','/': 'ظ',
                        ' ': ' '
 }
 
         
 def clip(text):
-    pyperclip.copy(text)
+    copy(text)
 
 def convEnToAr (text):
-    ArText = ''
+    ArText = ' '
     
     for char in range(0,len(text)):
         ArText +=EngToArKeyboard.get(text[char])
@@ -22,19 +23,19 @@ def convEnToAr (text):
     clip(ArText)
 def execute_code():
     
-    pyautogui.hotkey('ctrl', 'a')
-    pyautogui.hotkey('ctrl', 'x')
-    convEnToAr(pyperclip.paste())
-    pyautogui.hotkey('ctrl', 'v')
+    hotkey('ctrl', 'a')
+    hotkey('ctrl', 'x')
+    convEnToAr(paste().lower())
+    hotkey('ctrl', 'v')
 
 
 def keyboard_shortcut_handler():
-    keyboard.add_hotkey('ctrl+q', execute_code)     # Code to be executed when Ctrl+Q is pressed
-    keyboard.wait()
+    add_hotkey('ctrl+q', execute_code)     # Code to be executed when Ctrl+Q is pressed
+    wait()
 
 
 # Start the keyboard shortcut handler in a separate thread
-shortcut_thread = threading.Thread(target=keyboard_shortcut_handler)
+shortcut_thread = Thread(target=keyboard_shortcut_handler)
 shortcut_thread.start()
 
 # Wait for the shortcut thread to finish
